8aec74f3903998eb7187f9d2e4e20db52def541d
Christine <59668479+CTC118@users.noreply.github.com>
2020-06-09 13:38:52 +0200

    錯誤置入

--- result.php
+++
@@ -1,9 +0,0 @@
-<!-- 搜尋不到書籍的資料 -->
-<main >
-<h2 style="text-align: center;margin-top:15px;">Résutat de recherche</h2><hr>
-<div style=" text-align: center;margin-top:10px;">
-<span style="font-weight:500; color:red; font-size:1.25rem;"><?=$message?></span>
-
-</div>
-<br><br><br><br><br><br><br><br><br>
-</main>
